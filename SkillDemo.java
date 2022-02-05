f974f2aac99dc53257d51a57de9cde8362b407f1
Erwin Madjus <erwinmadjusbusiness@gmail.com>
2022-02-04 16:01:48 -0800

    new

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,6 +1,12 @@
 public class SkillDemo {
     public static int add(int n, int m) {
-        return n + m; 
+        if (n > m) {
+            return n + m; 
+        }
+        else if (n < m) {
+            return m - n;
+        }
+        else return 0; 
     }
 }
 
